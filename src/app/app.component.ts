52d6cf414f074117162d6763fddb2741826c5859
artes07 <artes07333@gmail.com>
2018-02-20 14:09:01 +0200

    add async validator for email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {reject} from 'q';
 
 @Component({
   selector: 'app-root',
@@ -22,7 +23,7 @@ export class AppComponent implements OnInit{
   ngOnInit() {
     this.form = new FormGroup({
       user: new FormGroup({
-        email: new FormControl('Email', [Validators.required, Validators.email]),
+        email: new FormControl('Email', [Validators.required, Validators.email], [this.checkForEmail]),
         pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)]),
       }),
       country: new FormControl('ru'),
@@ -41,6 +42,20 @@ export class AppComponent implements OnInit{
       };
     }
     return null;
+  }
+
+  checkForEmail(control: FormControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@mail.ru') {
+          resolve({
+            'emailIsUsed': true
+          });
+        } else {
+          resolve(null);
+        }
+      }, 3000);
+    });
   }
 
 }
